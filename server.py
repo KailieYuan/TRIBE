91fbe027de9935c553b6b2a412a543fababd6cb5
Kailie <kailie.yuan@cix.csi.cuny.edu>
2018-07-12 10:18:08 -0400

    need to get DB working

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 
 import os
 
-from jinja2 import StrictUndefined
+#from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 #from flask_debugtoolbar import DebugToolbarExtension
